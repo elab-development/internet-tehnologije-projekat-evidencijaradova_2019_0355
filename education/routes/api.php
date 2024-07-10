ff228317dd19876d47092f11b5db5d6bc87c2429
Anja Mitrovic <anja.mitrovic286@gmail.com>
2024-07-10 08:57:49 +0200

    manje izmene u routes

--- education/routes/api.php
+++ education/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Authorization\AuthController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/pg', [CourseController::class, 'indexPg']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -26,6 +31,9 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
+
+    Route::resource('courses', CourseController::class)
+        ->only(['store', 'update', 'destroy']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
